36606dc65a639ced904bda74a5f23ddee76a2286
maxiu271299 <maksymilian.kierski@gmail.com>
2020-03-19 17:49:30 +0100

    Add a mongoose and connection to the database

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+const dbConnectUrl = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(dbConnectUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('Connected to the DB 😎');
+  });
 
 const app = require('./app');
 
