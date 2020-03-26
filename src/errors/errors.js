113b66fd46ac32c5f67991e87dc3694259f7ffd6
s450026 <maksymilian.kierski@gmail.com>
2020-03-26 21:07:56 +0100

    Split error logging on development and production

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -10,16 +10,40 @@ class Errors extends Error {
   }
 }
 
+const errorDev = (error, res) => {
+  res.status(error.statusCode).json({
+    statusCode: error.statusCode,
+    status: error.status,
+    error: error,
+    message: error.message,
+    stack: error.stack
+  });
+};
+
+const errorProd = (error, res) => {
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message
+    });
+  } else {
+    console.error('ERROR💥', error);
+    res.status(500).json({
+      status: 'error',
+      message: 'I am sorry, something went wrong! ☹️'
+    });
+  }
+};
+
 //Global error handling middleware
 const errorHandler = (error, req, res, next) => {
   error.statusCode = error.statusCode || '500';
   error.status = error.status || 'error';
-  res.status(error.statusCode).json({
-    statusCode: error.statusCode,
-    status: error.status,
-    message: error.message,
-    stack: error.stack
-  });
+  if (process.env.NODE_ENV === 'development') {
+    errorDev(error, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    errorProd(error, res);
+  }
 };
 
 module.exports = {
