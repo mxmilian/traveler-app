efd81a4056833ddcd313f36ef817c3f9c30e2e15
s450026 <maksymilian.kierski@gmail.com>
2020-03-31 21:57:25 +0200

    Add a jwt to app, and add sign

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,24 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../errors/catchAsync');
 
 //Creating a new user
 const signUp = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  //const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
