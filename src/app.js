0b7b13c7e124dac1da3835c0e975319fa9fca4fd
s450026 <maksymilian.kierski@gmail.com>
2020-04-09 19:18:51 +0200

    Add data sanitization for xss, nosql injection and xss limit query body

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const { limitReq } = require('./middlewares/appProtect');
 
 const app = express();
 
+//Set security headers
 app.use(helmet());
-app.use(morgan('dev'));
+
+//Set limit req
 app.use('/api', limitReq);
-//This allows us to get access to the body from request
-app.use(express.json());
+
+//Read max 10kb data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+app.use(morgan('dev'));
+
+//Data sanitization NoSql query injection
+app.use(mongoSanitize());
+
+// Data sanitization xss
+app.use(xss());
 
 app.use((req, res, next) => {
   console.clear();
