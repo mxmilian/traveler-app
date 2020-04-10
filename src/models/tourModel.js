b7c419aaa4c0f43dc257c03fe03a258b5ce18857
s450026 <maksymilian.kierski@gmail.com>
2020-04-10 19:09:41 +0200

    Add a embeded model of locations in tour model

--- src/models/tourModel.js
+++ src/models/tourModel.js
@@ -76,7 +76,29 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    startLocation: {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
