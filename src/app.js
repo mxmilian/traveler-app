e50dfc14600dd465e873af3676b5822754a33a72
s450026 <maksymilian.kierski@gmail.com>
2020-04-10 13:41:11 +0200

    Add sanitization from parameter pollution

--- src/app.js
+++ src/app.js
@@ -3,27 +3,42 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const { limitReq } = require('./middlewares/appProtect');
 
 const app = express();
 
-//Set security headers
+// Set security headers
 app.use(helmet());
 
-//Set limit req
+// Set limit req
 app.use('/api', limitReq);
 
-//Read max 10kb data from body into req.body
+// Read max 10kb data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
 app.use(morgan('dev'));
 
-//Data sanitization NoSql query injection
+// Data sanitization NoSql query injection
 app.use(mongoSanitize());
 
 // Data sanitization xss
 app.use(xss());
+
+// Query sanitization from pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'difficulty',
+      'price',
+      'maxGroup'
+    ]
+  })
+);
 
 app.use((req, res, next) => {
   console.clear();
