0d6417e63d54103a6700a4f8131ee775d0eb185f
s450026 <maksymilian.kierski@gmail.com>
2020-04-16 19:43:44 +0200

    Add validation to unique review from user to particualr tour

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -36,6 +36,8 @@ const reviewSchema = mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function(next) {
   this.populate({
     path: 'user',
