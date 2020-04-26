326bbfba543dc9411fff6b9aa5586eaa62a58ffe
s450026 <maksymilian.kierski@gmail.com>
2020-04-26 23:24:56 +0200

    Set maxbox (markers)

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -5,7 +5,39 @@ console.log(locations);
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoibWF4aXUyNzEyOTkiLCJhIjoiY2s5aGh2ZDg4MDhjYzNtdGkwN3RvNTZucyJ9.HYPmad7VUIY_Vj8S1QupnQ';
-var map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
+  style: 'mapbox://styles/maxiu271299/ck9hiue6y177a1il7nrlzgth9'
+  // center: [ 17.5226874, 53.7000897],
+  // zoom: 10,
+  // interactive: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  //Create marker
+  const element = document.createElement('div');
+  element.className = 'marker';
+
+  //Add marker
+  new mapboxgl.Marker({
+    element,
+    anchor: 'bottom'
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+
+  //Extend marker
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 200,
+    left: 400,
+    right: 400
+  }
 });
