c608bd85ce7538c06bb89a7357bb58ca8b2646bc
s450026 <maksymilian.kierski@gmail.com>
2020-04-28 17:43:28 +0200

    Add checking is user currently logged middleware

--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { protectRoute } = require('../middlewares/authProtect');
+const { isLoggedCurrently } = require('../middlewares/authProtect');
 
 const {
   getHome,
@@ -11,9 +11,11 @@ const {
   getLogin
 } = require('../controllers/viewController');
 
+router.use(isLoggedCurrently);
+
 router.get('/', getHome);
 router.get('/tours', getTours);
-router.get('/tours/:slug', protectRoute, getTour);
+router.get('/tours/:slug', getTour);
 router.get('/login', getLogin);
 
 module.exports = router;
