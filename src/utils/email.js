2bd273e03dd4991c75c089cd0c18d740236e0d5c
s450026 <maksymilian.kierski@gmail.com>
2020-05-03 16:37:51 +0200

    Add class on which sending email to the client is based

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,25 +1,58 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.name = user.name;
+    this.url = url;
+    this.from = `Traveler app <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //SEND GRID
+      return 1;
     }
-  });
-  // 2) Define the email options
-  const emailOptions = {
-    from: 'Traveller app',
-    to: options.email,
-    subject: options.subject,
-    message: options.message
-  };
 
-  // 3) Actually send the email
-  await transporter.sendMail(emailOptions);
-};
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-module.exports = sendEmail;
+  async sendTemplate(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        name: this.name,
+        url: this.url,
+        subject
+      }
+    );
+
+    // 2) Define email option
+    const emailOptions = {
+      from: this.from,
+      to: this.to,
+      subject: subject,
+      html,
+      text: htmlToText.fromString(html)
+    };
+
+    // 3) Create a transport and send the email
+    await this.newTransport().sendMail(emailOptions);
+  }
+
+  async sendConfirm() {
+    await this.sendTemplate('confirm', 'Confirm email, and Welcome! 💜');
+  }
+}
+
+module.exports = Email;
